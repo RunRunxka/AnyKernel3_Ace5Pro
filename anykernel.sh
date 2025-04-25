3cbf9da426ba558af4ce290f3c44e6ea950d611d
aa123330 <844629699@qq.com>
2025-04-25 15:48:43 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -51,5 +51,4 @@ else
     dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
     write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 fi
-flash_dtbo
 ## end boot install
